fc8ef966c69b1aaf9e9c1d9de48aa53a313a80e5
Keegan DSouza (Collabera) <v-kedsou@microsoft.com>
2019-09-03 19:32:39 -0500

    adf

--- systemjs.config.js
+++ systemjs.config.js
@@ -6,7 +6,7 @@
     System.config({
         paths: {
             // paths serve as alias
-            'npm:': 'node_mffdgfdgdodules/'
+            'npm:': 'node_modules/'
         },
         // map tells the System loader where to look for things
         map: {
@@ -22,7 +22,6 @@
             '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
             '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
             '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-            'test' :'npm:test.js',
 
             // other libraries
             'rxjs': 'npm:rxjs',
