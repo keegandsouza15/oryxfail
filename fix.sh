5ae58b51d955c624426ed15cae69434a7ab1f326
Keegan D'Souza <keegan0415dsouza@gmail.com>
2019-09-04 17:42:10 -0500

    asdf

--- fix.sh
+++ fix.sh
@@ -4,6 +4,10 @@ pwd
 touch fromscript1.txt
 ln -s /node_modules /home/site/wwwroot/node_modules
 
-set 
 echo thescript is being run
 echo $DEPLOYMENT_TEMP
+ln -s $DEPLOYMENT_TEMP/node_modules ./node_modules 
+ln -s $DEPLOYMENT_TEMP/node_modules /home/site/wwwroot/node_modules
+ln -s $DEPLOYMENT_TEMP/node_modules ln -s $DEPLOYMENT_SOURCE/node_modules
+
+
