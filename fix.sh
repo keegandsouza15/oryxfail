1e63a0d8556ff4bebf55b465fcd2029940189e39
Keegan D'Souza <keegan0415dsouza@gmail.com>
2019-09-04 17:42:25 -0500

    sdaf

--- fix.sh
+++ fix.sh
@@ -8,6 +8,6 @@ echo thescript is being run
 echo $DEPLOYMENT_TEMP
 ln -s $DEPLOYMENT_TEMP/node_modules ./node_modules 
 ln -s $DEPLOYMENT_TEMP/node_modules /home/site/wwwroot/node_modules
-ln -s $DEPLOYMENT_TEMP/node_modules ln -s $DEPLOYMENT_SOURCE/node_modules
+ln -s $DEPLOYMENT_TEMP/node_modules $DEPLOYMENT_SOURCE/node_modules
 
 
