320592d07526a8c23d09b1a3828be6a060cfa031
Keegan D'Souza <keegan0415dsouza@gmail.com>
2019-09-04 17:46:26 -0500

    asf23

--- fix.sh
+++ fix.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 cd /home/site/wwwroot
 pwd
-touch fromscript1.txt
-ln -s /node_modules /home/site/wwwroot/node_modules
-
-echo thescript is being run
-echo $DEPLOYMENT_TEMP
-ln -s $DEPLOYMENT_TEMP/node_modules ./node_modules 
-ln -s $DEPLOYMENT_TEMP/node_modules /home/site/wwwroot/node_modules
-ln -s $DEPLOYMENT_TEMP/node_modules $DEPLOYMENT_SOURCE/node_modules
+cd /home/site
+touch site.txt
 
 
