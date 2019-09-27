2f0de1c6601dcde78d9fd408f5ba3d0e42542fc7
Keegan D'Souza <keegan0415dsouza@gmail.com>
2019-09-27 15:16:16 -0500

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 echo hello thdere
+ln -s /node_modules /home/site/wwwroot/node_modules
+
 node --version
 npm --version
 npm start
-ln -s /node_modules ./node_modules
+ln -s /node_modules /home/site/wwwroot/node_modules
+echo linking 
